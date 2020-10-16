47d474af78fedb3cd50f2a8cf817932edde1fcce
DaleFoley <dale.foley@live.com.au>
2020-10-16 11:29:03 +1100

    Init commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fileA = File.ReadAllText("../../JavaInstall.cpp");
+        string fileB = File.ReadAllText("../../JavaInstallList.cpp");
+
+        Diff diff = new Diff();
+        Diff.Item[] diffItems = diff.DiffText(fileA, fileB);
+
+        int diffItemLength = diffItems.Length;
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Original algorithm downloaded from: [http://www.mathertel.de/Downloads/Start_diff.aspx]
+
+An C# implementation of the difference algorithm published in "An O(ND) Difference Algorithm and its Variations" by Eugene Myers Algorithmica Vol. 1 No. 2, 1986, p 251.
+A detailed description for this diff class is a available at [http://www.mathertel.de/Diff/Default.aspx].
